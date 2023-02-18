1b2b104d33df2e31ef9e354e74a3aca5dc1f08de
yaroslav em <mr.emelyanenko@gmail.com>
2023-02-18 19:10:01 +0100

    Добавляет 3 из 4х функций для ДЗ№2

---
+++ js/functions.js
@@ -0,0 +1,31 @@
+//Проверка длины строки
+let string;
+let minLength;
+
+function checkLenght (string, minLength) {
+  return (string.length >= minLength) ? 'строка проходит по длине' : 'строка не проходит';
+}
+
+//Проверка на палиндром
+function checkPalindrome (string) {
+  string = string.toLowerCase().replace(/ /g,'');
+  const lastIndex = string.length - 1;
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] == string[lastIndex - i]) {
+      return console.log('Это палиндром');
+    }
+    return console.log('Это не палиндром');
+  }
+}
+
+//Извлечение чисел из строки
+
+
+function getNumbers (string) {
+  string = String(string);
+  string = string.replace(/[^0-9]/g, '');
+  if (string.length !== 0) {
+  return console.log(string);
+  }
+  return console.log('NaN');
+}
