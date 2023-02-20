4aae2ca4c74d9886cd21018856a72c74b85e1e8e
yaroslav em <mr.emelyanenko@gmail.com>
2023-02-20 19:51:08 +0100

    Исправлил часть ошибок по lint

--- js/functions.js
+++ js/functions.js
@@ -2,39 +2,39 @@
 const checkLenght = (string, length) => string.length >= length;
 
 //Проверка на палиндром
-function isPalindrom (string) {
+const isPalindrom = (string) => {
   string = string.toLowerCase().replace(/ /g,'');
   const lastIndex = string.length - 1;
   for (let i = 0; i < string.length / 2; i++) {
-    if (string[i] == string[lastIndex - i]) {
+    if (string[i] === string[lastIndex - i]) {
       return 'Это палиндром';
     }
     return 'Это не палиндром';
   }
-}
+};
 
 const isPalindrome = (string) => {
   const tempString = string
     .toLowerCase()
     .replaceAll(' ', ''); //заменяем  все пробелы на пустой символ, тем самым избавляясь от пробелов
 
-  let reverseString ='';
+  let reverseString = '';
   for (let i = tempString.length - 1; i >= 0; i--) {
     reverseString += tempString.at(i);
     return tempString === reverseString;
   }
-}
+};
 
 //Извлечение цифр из строки
 //Вариант №1
-function extractNumber (string) {
+const extractNumber = (string) => {
   string = String(string);
   string = string.replace(/[^0-9]/g, '');
   if (string.length !== 0) {
-  return parseInt(string);
+    return parseInt (string);
   }
   return NaN;
-}
+};
 
 //Вариант №2
 const extrNumber = (string) => {
@@ -43,26 +43,15 @@ const extrNumber = (string) => {
   }
   let result = '';
   for (let i = 0; i < string.length; i++) {
-    if (!Number.isNaN(parseInt(string.at(i), 10)))
-    result += string.at(i);
+    if (!Number.isNaN(parseInt(string.at(i), 10))) {
+      result += string.at(i);
+    }
   }
   return parseInt(result, 10);
-}
+};
 
 //Функциия с 3 параметрами
-//Вариант №1 (не выполняет одно из условий)
-function noPad (string, targetLength, extraString) {
-  let extraLenght = targetLength - string.length;
-  if (string.length > targetLength) {
-    return string;
-  }
-  var extraString = extraString.repeat(extraLenght);
-  var newString = extraString.slice(0, extraLenght) + string;
-  return newString;
-}
-
-
-//Вариант №2 Полифил
+//Вариант №1 Полифил
 const myPadStart = (string, minLength, pad) => {
   const actualPad = minLength - string.length;
   if (actualPad <= 0) {
@@ -70,16 +59,16 @@ const myPadStart = (string, minLength, pad) => {
   }
   const tempPad = pad.slice(0, actualPad % pad.length);
   const tempRepeat = pad.repeat(actualPad / pad.length);
-  return tempPad  + tempRepeat + string;
-}
+  return tempPad + tempRepeat + string;
+};
 
-//Вариант №3 через циклы
+//Вариант №2 через циклы
 const PadStart = (string, minLength, pad) => {
   let result = string;
   while (result.length < minLength) {
     const newResultLenght = result.length + pad.length;
-    const actualPad = newResultLength <= minLength ? pad : pad.slice(0, minLength - newResultLenght);
+    const actualPad = newResultLenght <= minLength ? pad : pad.slice(0, minLength - newResultLenght);
     result = actualPad + result;
   }
   return result;
-}
+};
