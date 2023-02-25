6ed62be9c6c65cebda0963d4bc3c660f07dd5756
Keks <keks@htmlacademy.ru>
2023-02-26 01:25:44 +0300

    :heavy_check_mark: Сборка #3

---
+++ 3/js/functions.js
@@ -0,0 +1,18 @@
+const checkLength = (string, length) => string.length <= length;
+
+const isPalindrome = (string) => {
+  string = string.toLowerCase().replace(/\s/g, '');
+  return string === string.split('').reverse().join('');
+};
+
+const extractNumber = (string) => {
+  string = String(string).replace(/[^0-9]/g, '');
+  return parseInt(string, 10);
+};
+
+const createNewString = (string, minLength, additionalText) => {
+  while (string.length < minLength) {
+    string = additionalText.slice(0, minLength - string.length) + string;
+  }
+  return string;
+};
