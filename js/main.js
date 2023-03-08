defe73a0fa2be03417a2974d26d7d141cc967265
yaroslav em <mr.emelyanenko@gmail.com>
2023-03-08 14:51:56 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
 import { renderPictures } from './thumbnails.js';
 import { hideObjects } from './bigpicture.js';
+import { createComment } from './bigpicture.js';
 
 renderPictures();
 hideObjects();
+createComment();
