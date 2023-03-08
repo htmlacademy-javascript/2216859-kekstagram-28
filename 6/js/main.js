d56bd9a528f9263c34819c23db59e7f4927b05ad
Keks <keks@htmlacademy.ru>
2023-03-08 23:07:20 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/main.js
@@ -0,0 +1,7 @@
+import { renderPictures } from './thumbnails.js';
+import { hideObjects } from './bigpicture.js';
+import { getComment } from './bigpicture.js';
+
+renderPictures();
+hideObjects();
+getComment();
---
+++ 6/js/thumbnails.js
@@ -0,0 +1,25 @@
+import { createPictures } from './data.js';
+import { openBigPicture } from './bigpicture.js';
+
+const thumbnailsList = document.querySelector('.pictures');
+const thumbnailTemplate = document.querySelector('#picture').content.querySelector('.picture');
+const pictureData = createPictures();
+
+
+const createPicture = (picture) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+  thumbnail.querySelector('.picture__img').src = picture.url;
+  thumbnail.querySelector('.picture__img').alt = picture.description;
+  thumbnail.querySelector('.picture__likes').textContent = picture.likes;
+  thumbnail.querySelector('.picture__comments').textContent = picture.comments.length;
+  thumbnail.addEventListener('click', () => {
+    openBigPicture(picture);
+  });
+  return thumbnail;
+};
+
+const renderPictures = () => {
+  pictureData.forEach((item) => thumbnailsList.appendChild(createPicture(item)));
+};
+
+export { renderPictures };
