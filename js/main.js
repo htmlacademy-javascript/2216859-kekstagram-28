21b17719e5e7f450a829e3becc0f17ed13df4f80
yaroslav em <mr.emelyanenko@gmail.com>
2023-03-17 11:45:46 +0100

    Реализовал открытие и закрытие формы + ESC

--- js/main.js
+++ js/main.js
@@ -1,3 +1,5 @@
 import { renderPictures } from './thumbnails.js';
+import { openUserForm } from './user-form.js';
 
 renderPictures();
+openUserForm();
--- js/user-form.js
+++ js/user-form.js
@@ -1,11 +1,43 @@
+import { isEscapeKey } from './util.js';
+const form = document.querySelector('#upload-select-image');
+
+new Pristine(form);
 const userForm = document.querySelector('.img-upload__overlay');
-const uploadFile = document.querySelector('.img-upload__label');
+const uploadFileBtn = document.querySelector('#upload-file');
+const closeFormBtn = document.querySelector('#upload-cancel');
 
-uploadFile.addEventListener('click', onUploadFileClick);
-
-const onUploadFileClick = (event) => {
-  event.preventDefault();
+const onUploadClick = (event) => {
   userForm.classList.remove('hidden');
+  document.body.classList.add('modal-open');
+  event.preventDefault();
+  openUserForm();
 };
 
+const onFormClick = (event) => {
+  event.preventDefault();
+  closeForm();
+};
+
+const onDocumentKeyDown = (event) => {
+  if (isEscapeKey(event)) {
+    event.preventDefault();
+    closeForm();
+  }
+};
+
+const openUserForm = () => {
+  uploadFileBtn.addEventListener('change', onUploadClick);
+  closeFormBtn.addEventListener('click', onFormClick);
+  document.addEventListener('keydown', onDocumentKeyDown);
+};
+
+const closeForm = () => {
+  userForm.classList.add('hidden');
+  document.body.classList.remove('modal-open');
+  closeFormBtn.removeEventListener('click', onFormClick);
+  document.removeEventListener('keydown', onDocumentKeyDown);
+  uploadFileBtn.removeEventListener('change', onFormClick);
+};
+
+export { openUserForm };
 
