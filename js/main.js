c26a946bf9c4ecd637ff07d0fa32ea2814dc0ece
yaroslav em <mr.emelyanenko@gmail.com>
2023-03-23 10:01:26 +0100

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import { renderPictures } from './thumbnails.js';
-import { openUserForm } from './user-form.js';
+import { addUserFormAction } from './user-form.js';
 
 renderPictures();
-openUserForm();
+addUserFormAction();
